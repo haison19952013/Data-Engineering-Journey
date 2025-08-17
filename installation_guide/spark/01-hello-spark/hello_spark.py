4c6de96080c84e80f24684955d5aae0917cdfaa6
haison19952013 <haison19952013@gmail.com>
2025-08-17 11:07:49 +0700

    add comment

--- installation_guide/spark/01-hello-spark/hello_spark.py
+++ installation_guide/spark/01-hello-spark/hello_spark.py
@@ -1,6 +1,8 @@
 from pyspark.sql import *
 
 if __name__ == "__main__":
+    # Create a Spark session with the application name "HelloSpark"
+    # and connect to the Spark master at spark://spark:7070
     spark = SparkSession.builder \
         .appName("HelloSpark") \
         .master("spark://spark:7077") \
@@ -11,7 +13,10 @@ if __name__ == "__main__":
     data_list = [("Phuong", 31),
                  ("Huy", 31),
                  ("Bee", 25)]
-
+    # Create a DataFrame from the list of tuples
+    # and name the columns "Name" and "Age"
+    # The DataFrame will be displayed in a tabular format
+    # when df.show() is called
     df = spark.createDataFrame(data_list).toDF("Name", "Age")
     df.show()
 
